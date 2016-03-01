e0f119c4c881d993b3d86371e078961a4e41622f
Stanislav Lesnikov <stan@nashdot.com>
2016-03-02 00:03:18 +0100

    Add getLocation and mapSequence helpers

---
+++ app/utils/mapSequence.js
@@ -0,0 +1,16 @@
+export default function mapSequence(array, fn) {
+  const results = [];
+  let promise = Promise.resolve();
+
+  function next(member, i) {
+    return fn(member).then(value => {
+      results[i] = value;
+    });
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    promise = promise.then(() => next(array[i], i));
+  }
+
+  return promise.then(() => results);
+}
