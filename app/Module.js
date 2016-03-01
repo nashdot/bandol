d6c5f75234fcc9ab8e304c1c13615fe4e231e6e9
Stanislav Lesnikov <stan@nashdot.com>
2016-03-02 00:03:30 +0100

    Add Module class

---
+++ app/Module.js
@@ -0,0 +1,58 @@
+import { blank } from './utils/object.js';
+import getLocation from './utils/getLocation.js';
+import traverse from 'babel-traverse';
+// import * as types from 'babel-types';
+
+export default class Module {
+  constructor({ id: id, code: code, ast: ast, bundle: bundle }) {
+    this.id = id;
+    this.code = code;
+    this.ast = ast;
+    this.bundle = bundle;
+
+    this.sources = [];
+    this.imports = blank();
+  }
+
+  addImport(node) {
+    const source = node.source.value;
+
+    if (!~this.sources.indexOf(source)) {
+      this.sources.push(source);
+    }
+
+    node.specifiers.forEach(specifier => {
+      const localName = specifier.local.name;
+
+      if (this.imports[localName]) {
+        const err = new Error(`Duplicated import '${localName}'`);
+        err.file = this.id;
+        err.loc = getLocation(this.code, specifier.start);
+        throw err;
+      }
+
+      const isDefault = specifier.type === 'ImportDefaultSpecifier';
+      const isNamespace = specifier.type === 'ImportNamespaceSpecifier';
+
+      let name;
+      if (isDefault) {
+        name = 'default';
+      } else {
+        if (isNamespace) {
+          name = '*';
+        } else {
+          name = specifier.imported.name;
+        }
+      }
+      this.imports[localName] = { source: source, name: name, module: null };
+    });
+  }
+
+  parse() {
+    traverse(this.ast, {
+      ImportDeclaration: (path) => {
+        this.addImport(path.node);
+      }
+    });
+  }
+}
