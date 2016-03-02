0e7af440b1718c60e495d6d5ace1bdc028c2d7f9
Stanislav Lesnikov <stan@nashdot.com>
2016-03-02 02:04:49 +0100

    Tests: add module test

---
+++ tests/fixtures/core/module/actual.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+
+
+render(
+    <h1>Hallo Welt</h1>,
+    document.getElementById('content'));
