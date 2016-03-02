eae6a558ce0346df653afe09e5463014750e9a62
Stanislav Lesnikov <stan@nashdot.com>
2016-03-02 02:28:14 +0100

    lint

--- tests/fixtures/core/module/actual.js
+++ tests/fixtures/core/module/actual.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 
-
 render(
     <h1>Hallo Welt</h1>,
     document.getElementById('content'));
