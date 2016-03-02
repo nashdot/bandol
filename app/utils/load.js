b2b4fc3bb78a2e40f42f030d69f51d125a3a8293
Stanislav Lesnikov <stan@nashdot.com>
2016-03-02 12:40:48 +0100

    Remove console.log

--- app/utils/load.js
+++ app/utils/load.js
@@ -26,16 +26,13 @@ const babylonOtions = {
 };
 
 export default function load(id) {
-  console.log(`Loading '${id}'`);
   return new Promise((resolve, reject) => {
     fs.readFile(id, 'utf8', (err, data) => {
       if (err) {
-        console.log(`Loading error: '${err}'`);
         reject(err);
       }
 
       const ast = babylon.parse(data, babylonOtions);
-      console.log(`Loading parsed`);
       resolve({ code: data, ast: ast });
     });
   });
