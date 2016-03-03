348925855231b8bfc4b8ef0144d76db91346ef52
Stanislav Lesnikov <stan@nashdot.com>
2016-03-03 16:39:04 +0100

    Use plugin system to load files

---
+++ app/plugins/bandol-plugin-resolve-npm/index.js
@@ -0,0 +1,22 @@
+import resolveModule from 'resolve';
+import path from 'path';
+
+export default function () {
+  return {
+    resolveResource: (importee, importer) => {
+      return new Promise((resolve, reject) => {
+        let baseDir;
+        if (importer === undefined) {
+          baseDir = process.cwd();
+        } else {
+          baseDir = path.dirname(importer);
+        }
+
+        resolveModule(importee, { basedir: baseDir }, (err, res) => {
+          if (err) reject(err);
+          else resolve(res);
+        });
+      });
+    }
+  };
+}
