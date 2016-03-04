d7f63a7c0aba97f0ca5469ff8dcc5538072f257c
Stanislav Lesnikov <stan@nashdot.com>
2016-03-04 12:54:22 +0100

    Make resolver sync

--- app/plugins/bandol-plugin-resolver-node/index.js
+++ app/plugins/bandol-plugin-resolver-node/index.js
@@ -10,18 +10,13 @@ export default class Plugin extends BasePlugin {
   }
 
   resolveResource(importee, importer) {
-    return new Promise((resolve, reject) => {
-      let baseDir;
-      if (importer === undefined) {
-        baseDir = process.cwd();
-      } else {
-        baseDir = path.dirname(importer);
-      }
+    let baseDir;
+    if (importer === undefined) {
+      baseDir = process.cwd();
+    } else {
+      baseDir = path.dirname(importer);
+    }
 
-      resolveModule(importee, { basedir: baseDir }, (err, res) => {
-        if (err) reject(err);
-        else resolve(res);
-      });
-    });
+    return resolveModule.sync(importee, { basedir: baseDir });
   }
 }
