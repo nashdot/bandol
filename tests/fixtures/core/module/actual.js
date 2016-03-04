532c05c203136c43c8244e9aa2f40a1b14f44ad3
Stanislav Lesnikov <stan@nashdot.com>
2016-03-04 18:03:06 +0100

    Don't use JSX syntax (for this moment)

--- tests/fixtures/core/module/actual.js
+++ tests/fixtures/core/module/actual.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 
+
 render(
-    <h1>Hallo Welt</h1>,
-    document.getElementById('content'));
+  React.createElement(
+    'h1',
+    null,
+    'Hallo Welt'
+  ), document.getElementById('content'));
