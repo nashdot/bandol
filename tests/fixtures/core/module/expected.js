bbba2c9b3568e0b058f4accd79c1d35d10345dc9
Stanislav Lesnikov <stan@nashdot.com>
2016-03-10 07:53:48 +0100

    Correct expected test

--- tests/fixtures/core/module/expected.js
+++ tests/fixtures/core/module/expected.js
@@ -1,10 +1,5 @@
-import React from 'react/lib/React';
-import { render } from 'react/lib/ReactDOM';
-import { Router, Route, hashHistory } from 'react-router/es6';
+import 'react/lib/React';
+import 'react/lib/ReactDOM';
+import 'react-router/es6';
 
-render(
-  React.createElement(
-    'h1',
-    null,
-    'Hallo Welt'
-  ), document.getElementById('content'));
+render(React.createElement('h1', null, 'Hallo Welt'), document.getElementById('content'));
