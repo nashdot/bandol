bfdb1fdc236940dac410ee51f0a1555b87e7e8a8
Stanislav Lesnikov <stan@nashdot.com>
2016-03-18 10:26:39 +0100

    Add named-members optimiser plugin

---
+++ app/plugins/bandol-plugin-optimizer-named-members/index.js
@@ -0,0 +1,30 @@
+import traverse from 'babel-traverse';
+
+import BasePlugin from '../../BasePlugin';
+import Types from '../../Types';
+
+export default class Plugin extends BasePlugin {
+  name = 'optimizer-named-members';
+  version = '0.1.0';
+  resourceType = Types.JAVASCRIPT;
+
+  supportedExtensions = ['.js', '.jsx', '.es6', '.es'];
+
+  constructor(bundle) {
+    super(bundle);
+    this.bundle = bundle;
+  }
+
+  optimizeBundle() {
+    for (let i = this.bundle.sortedResources.length - 1; i >= 0; i--) {
+      const resource = this.bundle.sortedResources[i];
+
+      if (!this.isSupportedExtension(resource.id)
+        && resource.type !== this.resourceType) {
+        this.log(`Can't optimize ${resource.id}`);
+      } else {
+        // TODO
+      }
+    }
+  }
+}
