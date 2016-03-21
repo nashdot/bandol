4cfb3fd8fd357e42da4a08f8e5ceabcf7040457b
Stanislav Lesnikov <stan@nashdot.com>
2016-03-21 23:03:54 +0100

    Simplify React test example

--- tests/fixtures/core/module/actual.js
+++ tests/fixtures/core/module/actual.js
@@ -1,8 +1,8 @@
 import React from 'react/lib/React';
-import ReactDOM from 'react/lib/ReactDOM';
+import { render } from 'react/lib/ReactDOM';
 import { Router, Route, hashHistory } from 'react-router/es6';
 
-ReactDOM.render(
+render(
   React.createElement(
     'h1',
     null,
