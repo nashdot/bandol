75ff406e780d9fdca1118df7dccb87ba1c1050aa
Stanislav Lesnikov <stan@nashdot.com>
2016-03-26 12:08:04 +0100

    Correct loader

--- app/plugins/bandol-plugin-loader-js/index.js
+++ app/plugins/bandol-plugin-loader-js/index.js
@@ -45,7 +45,7 @@ export default class Plugin extends BasePlugin {
   loadResource(resource) {
     return new Promise((resolve) => {
       if (!this.isSupportedExtension(resource.id)
-        && resource.type !== Types.UNKNOWN) {
+        || resource.type !== Types.UNKNOWN) {
         this.log(`Can't load ${resource.id}`);
         resolve(resource);
       } else {
