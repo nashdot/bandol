4e6ca06a8c79b0277d3e17be972454d1e1dc053b
Stanislav Lesnikov <stan@nashdot.com>
2016-05-08 08:18:18 +0200

    Rename function locals if used by globals

--- app/plugins/bandol-plugin-optimizer-rename-internals/index.js
+++ app/plugins/bandol-plugin-optimizer-rename-internals/index.js
@@ -16,13 +16,22 @@ export default class Plugin extends BasePlugin {
     for (let i = this.bundle.sortedResources.length - 1; i >= 0; i--) {
       const resource = this.bundle.sortedResources[i];
 
-      // Rename global but internally used variables
       traverse(resource.ast, {
+        // Rename global but internally used variables
         Program: (nodePath) => {
           Object.keys(nodePath.scope.bindings).forEach((bindingName) => {
             const binding = nodePath.scope.bindings[bindingName];
             if (binding.kind !== 'module'
                 && !this.bundle.hasName(bindingName)) {
+              const newName = this.bundle.generateUid();
+              nodePath.scope.rename(bindingName, newName);
+            }
+          });
+        },
+        // Rename function's local variables if already used by globals
+        FunctionDeclaration: (nodePath) => {
+          Object.keys(nodePath.scope.bindings).forEach((bindingName) => {
+            if (this.bundle.hasName(bindingName)) {
               const newName = this.bundle.generateUid();
               nodePath.scope.rename(bindingName, newName);
             }
